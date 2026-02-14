adf1b92dfd7eb7e3d5d3e810b62ef52ae94f9206
Raed854 <nefziraed9@gmail.com>
2026-02-14 10:31:42 +0100

    Save before Angular downgrade

--- MemorIA_Frontend/src/app/app.routes.ts
+++ MemorIA_Frontend/src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { DiagnosticComponent } from './diagnostic/diagnostic.component';
+import { RapportComponent } from './rapport/rapport.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'home', component: HomeComponent }
+  { path: 'home', component: HomeComponent },
+  { path: 'diagnostic', component: DiagnosticComponent },
+  { path: 'rapport', component: RapportComponent }
 ];
